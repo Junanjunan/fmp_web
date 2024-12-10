fb5652665d897b60ed4d86a17dbf13a983bc8b11
Junanjunan <taltalrealty@gmail.com>
2024-12-10 22:47:05 +0900

    types: [fix] SymbolRow interface with Foreign key tables' interface

--- types/db.ts
+++ types/db.ts
@@ -1,4 +1,19 @@
+export interface TypeRow {
+    id: string;
+}
+
+export interface ExchangeRow {
+    id: string;
+    name: string;
+}
+
 export interface SymbolRow {
     id: string;
     name: string;
+    price: number;
+    type_id: TypeRow["id"];
+    exchange_id: ExchangeRow["id"];
+    is_updated: boolean;
+    is_existing: boolean;
+    updated_at: Date;
 }
