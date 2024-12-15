267830ed81ad7a2be737bc63d1d7b12be9f46117
Junanjunan <taltalrealty@gmail.com>
2024-12-15 18:56:43 +0900

    config: [add] Divide variables based on running condition

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,16 @@
 import type { NextConfig } from "next";
 
+const env = process.env;
+const isProduction = env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   env: {
-    DB_USER: process.env.DB_USER,
-    DB_HOST: process.env.DB_HOST,
-    DB_NAME: process.env.DB_NAME,
-    DB_PASSWORD: process.env.DB_PASSWORD,
-    DB_PORT: process.env.DB_PORT,
+    DB_USER: isProduction ? env.PRODUCTION_DB_USER : env.LOCAL_DB_USER,
+    DB_HOST: isProduction ? env.PRODUCTION_DB_HOST : env.LOCAL_DB_HOST,
+    DB_NAME: isProduction ? env.PRODUCTION_DB_NAME : env.LOCAL_DB_NAME,
+    DB_PASSWORD: isProduction ? env.PRODUCTION_DB_PASSWORD : env.LOCAL_DB_PASSWORD,
+    DB_PORT: isProduction ? env.PRODUCTION_DB_PORT : env.LOCAL_DB_PORT,
+    SERVER_URL: isProduction ? env.PRODUCTION_SERVER_URL : env.LOCAL_SERVER_URL,
   }
 };
 
