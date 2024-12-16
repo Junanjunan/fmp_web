a14126777f8e0acdaf29f9a88a7a91ac36df2255
Junanjunan <taltalrealty@gmail.com>
2024-12-16 20:59:27 +0900

    math: [fix] Make return specific value about growth when previous revenue is 0

--- lib/math.ts
+++ lib/math.ts
@@ -2,6 +2,13 @@ export const getGrowth = (
   currentYearRevenue: number, 
   previousYearRevenue: number
 ): number => {
+  if (!previousYearRevenue) {
+    if (currentYearRevenue) {
+      return 999.99;
+    } else{
+      return -999.99;
+    }
+  }
   const diff = currentYearRevenue - previousYearRevenue;
   let growth = diff / previousYearRevenue * 100;
   growth = parseFloat(growth.toFixed(2));
