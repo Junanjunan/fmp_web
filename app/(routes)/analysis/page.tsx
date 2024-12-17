adacc6d08cf950c12afae6326da851104d2df8b9
Junanjunan <taltalrealty@gmail.com>
2024-12-17 22:48:06 +0900

    analysis: [refactor] Table of filtered revenue

--- app/(routes)/analysis/page.tsx
+++ app/(routes)/analysis/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from 'react';
 import { CheckboxList, Button, Select } from '@/app/components/UI';
+import { RevenueTable } from '@/app/components/client/IncomeStatement';
 import { requestGet, requestAnalysis } from '@/app/axios';
 import { TypeRow, ExchangeRow, GrowthOfSymbols } from '@/types';
 
@@ -85,46 +86,12 @@ const AnalysisPage = () => {
           className="border border-gray-300"
         />
       </div>
-      <table className="min-w-full border border-gray-300 mt-4">
-        <thead>
-          <tr className="bg-gray-200">
-            <th className="border border-gray-300 px-4 py-2">Growth (%)</th>
-            {filteredYears.map((year) => (
-              <th key={year} className="border border-gray-300 px-4 py-2">{year}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {Object.entries(symbolGrowths).map(([symbol, growthArray]) => {
-            const thirdYear = Number(years[2]);
-            const yearsOfSymbol = growthArray.map(growth => growth.year);
-            if (!yearsOfSymbol.includes(thirdYear)) {
-              return null;
-            }
-
-            for (let i = 0; i < growthArray.length; i++) {
-              for (const year of years) {
-                if (growthArray[i].year == year) {
-                  if (!growthArray[i].growth || growthArray[i].growth < minimumGrowth) {
-                    return null;
-                  }
-                }
-              }
-            }
-
-            return (
-              <tr key={symbol}>
-                <td className="border border-gray-300 px-4 py-2">{symbol}</td>
-                {filteredYears.map((year) => (
-                  <td key={year} className="border border-gray-300 px-4 py-2">
-                    {growthArray.find(growth => growth.year == year)?.growth}
-                  </td>
-                ))}
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
+      <RevenueTable
+        filteredYears={filteredYears}
+        symbolGrowths={symbolGrowths}
+        years={years}
+        minimumGrowth={minimumGrowth}
+      />
     </main>
   );
 };
