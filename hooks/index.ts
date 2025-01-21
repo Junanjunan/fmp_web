c853a74451b196949178a922be2e824297771adf
Junanjunan <taltalrealty@gmail.com>
2025-01-21 20:36:52 +0900

    Watchlist: [fix] Show color of symbol rows that in watchlists

--- hooks/index.ts
+++ hooks/index.ts
@@ -2,10 +2,10 @@ import { useEffect, useRef, useState } from 'react';
 import { useSession } from 'next-auth/react';
 import { useWatchlistStore } from '@/app/stores/useStore';
 import { requestGetWatchlist } from '@/app/axios';
-import { OrgnizedWatchlistsObject } from '@/types/db';
+import { OrgnizedWatchlistsObject, SymbolRow } from '@/types/db';
 
 export const useWatchlistData = () => {
-  const { setWatchlistObject } = useWatchlistStore();
+  const { setWatchlistObject, setWatchlist } = useWatchlistStore();
   const { data: session } = useSession();
   const isDataFetched = useRef(false);
 
@@ -22,15 +22,18 @@ export const useWatchlistData = () => {
       .then((res) => {
         const allWatchlists = res.allWatchlists;
         const organizedWatchlists: OrgnizedWatchlistsObject = {};
+        const symbolsInWatchlists: SymbolRow["id"][] = [];
         allWatchlists.forEach((watchlist) => {
           if (!organizedWatchlists[watchlist.name]) {
             organizedWatchlists[watchlist.name] = [];
           }
           watchlist.user_symbols.forEach((userSymbol) => {
             organizedWatchlists[watchlist.name].push(userSymbol.symbol_id);
+            symbolsInWatchlists.push(userSymbol.symbol_id);
           });
         });
         setWatchlistObject(organizedWatchlists);
+        setWatchlist(symbolsInWatchlists);
       });
   }, []);
 };
